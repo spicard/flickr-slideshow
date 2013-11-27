5f9198ae65eef464201eeeb227385b7647c13606
Andre Marques <andremarquesweb@gmail.com>
2013-11-27 14:59:38 -0200

    thumbnails complete

--- functions.js
+++ functions.js
@@ -7,42 +7,76 @@ function Slide() {
 	this.init = function() {
 		self = this;
 		self.values();
-		self.clickButtonPrev();
-		self.clickButtonNext();
+		/*self.clickButtonPrev();
+		self.clickButtonNext();*/
 	}
 
 	this.values = function() {
-		self.list = document.querySelector('.content-slideshow ul');
-		self.item = document.querySelector('.content-slideshow ul li');
-		self.itensLength = document.querySelectorAll('.content-slideshow ul li').length;
-			
-		self.itemWidth = self.item.offsetWidth;
-		self.list.style.width = (self.itensLength * self.itemWidth) + 'px';
 		self.btnPrev = document.querySelector('.arrow.prev');
 		self.btnNext = document.querySelector('.arrow.next');
-		self.itemAtual = 0;
 
 		$.ajax({
-			url: 'http://www.flickr.com/services/rest/',
+			url: 'http://www.flickr.com/services/rest',
 			dataType: 'JSONP',
+			jsonp: 'jsoncallback',
 			data: {
-				method: 'flickr.people.getPhotos',
-				api_key: '42f8d9b0b14f40b5e5d265659d34c5a1',
-				user_id: '32596304%40N08',
-				format: 'JSONP',
-				api_sig: '0e8fed83c9f6ae5deaa6117122aabe55'
+				method: 'flickr.photos.search',
+				api_key: 'df6f4b524cf35757c57c6f7418e11d01',
+				user_id: '26982917@N02',
+				format: 'json',
 			},
 			success: function(response){
-				console.log('oi');
+				var quantItens = response.photos.photo.length;
+				var firstItem = response.photos.photo[0];
+				var farmId = firstItem.farm;
+				var serverId = firstItem.server;
+				var id = firstItem.id;
+				var secret = firstItem.secret;
+				var small = '_s.jpg';
+				var medium = '_z.jpg';
+				var url = 'http://farm' + farmId + '.staticflickr.com/' + serverId+ '/' + id+ '_' + secret;
+				var urlComplete = url + medium;
+
+				document.querySelector('.content-slideshow').innerHTML = '<img src"" />';
+				document.querySelector('.content-slideshow img').setAttribute('src', urlComplete);
+				document.querySelector('.content-thumbnails').innerHTML = '<ul></ul>';
+
+				for (var i = 0; i < quantItens; i++) {
+					var item = document.createElement('li');
+					var link = document.createElement('a');
+					var photo = response.photos.photo[i];
+					var farmId = photo.farm;
+					var serverId = photo.server;
+					var id = photo.id;
+					var secret = photo.secret;
+					var url = 'http://farm' + farmId + '.staticflickr.com/' + serverId+ '/' + id+ '_' + secret;
+
+					document.querySelector('.content-thumbnails ul').appendChild(item);
+					item.appendChild(link);
+					link.setAttribute('href', url + medium);
+					link.innerHTML = '<img src="' + url + small + '" />';
+					console.log(link)
+				};
+
+				var allThumbs = document.querySelectorAll('.content-thumbnails ul li a'),
+					allThumbsArray = [];
+
+				for (var i = 0; i < allThumbs.length; i++) {
+					allThumbsArray.push(allThumbs[i]);
+					allThumbs[i].addEventListener('click', function(e) {
+						var url = this.getAttribute('href');
+						document.querySelector('.content-slideshow img').setAttribute('src', url);
+						e.preventDefault();
+					})
+				};
 			}
 		});
 	}
 
-	this.clickButtonPrev = function() {
+	/*this.clickButtonPrev = function() {
 		self.btnPrev.addEventListener('click', function() {
 			if (self.itemAtual > 0) {
-				--self.itemAtual;
-				self.list.style.marginLeft = -(self.itemAtual * self.itemWidth)  + 'px';
+				document.querySelector('.content-slideshow img').setAttribute('src', url);
 			}
 		});
 	}
@@ -51,10 +85,10 @@ function Slide() {
 		self.btnNext.addEventListener('click', function() {
 			if (self.itemAtual < self.itensLength -1) {
 				++self.itemAtual;
-				self.list.style.marginLeft = -(self.itemAtual * self.itemWidth)  + 'px';
+				document.querySelector('.content-slideshow img').setAttribute('src', url);
 			}
 		});
-	}
+	}*/
 
 	this.init();
 }
