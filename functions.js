13889e86de05d2a4a4422d33e8ffc464fa5955e6
Andre Marques <andremarquesweb@gmail.com>
2013-12-04 14:33:36 -0200

    finish buttons prev and Next in slideshow

--- functions.js
+++ functions.js
@@ -7,121 +7,116 @@ function Slide() {
 	this.init = function() {
 		self = this;
 		self.values();
+		self.ajax()
 	}
 
 	this.values = function() {
 		self.btnPrev = document.querySelector('.arrow.prev');
 		self.btnNext = document.querySelector('.arrow.next');
+		self.currentItem = 0;
 
-		$.ajax({
-			url: 'http://www.flickr.com/services/rest',
-			dataType: 'JSONP',
-			jsonp: 'jsoncallback',
-			data: {
-				method: 'flickr.photos.search',
-				api_key: 'df6f4b524cf35757c57c6f7418e11d01',
-				user_id: '26982917@N02',
-				format: 'json',
-			},
-			success: function(response){
-				var quantItens = response.photos.photo.length;
-				var firstItem = response.photos.photo[0];
-				var farmId = firstItem.farm;
-				var serverId = firstItem.server;
-				var id = firstItem.id;
-				var secret = firstItem.secret;
-				var small = '_s.jpg';
-				var medium = '_z.jpg';
-				var url = 'http://farm' + farmId + '.staticflickr.com/' + serverId+ '/' + id+ '_' + secret;
-				var urlComplete = url + medium;
-
-				document.querySelector('.content-slideshow').innerHTML = '<ul></ul>';
-				document.querySelector('.content-thumbnails').innerHTML = '<ul></ul>';
-
-				for (var i = 0; i < quantItens; i++) {
-					var item = document.createElement('li');
-					var photo = response.photos.photo[i];
-					var farmId = photo.farm;
-					var serverId = photo.server;
-					var id = photo.id;
-					var secret = photo.secret;
-					var url = 'http://farm' + farmId + '.staticflickr.com/' + serverId+ '/' + id+ '_' + secret;
-					var currentItem = 0;
-
-					document.querySelector('.content-slideshow ul').appendChild(item);
-					item.innerHTML = '<img src="' + url + medium + '" />';
-				};
-				var itemWidth = document.querySelector('.content-slideshow ul li').offsetWidth;
-				document.querySelector('.content-slideshow ul').style.width = itemWidth * quantItens + 'px';
-
-				this.clickButtonPrev = function() {
-					var itemWidth = document.querySelector('.content-slideshow ul li').offsetWidth;
-					self.btnPrev.addEventListener('click', function() {
-						if (currentItem > 0) {
-							--currentItem;
-							document.querySelector('.content-slideshow ul').style.marginLeft = -(itemWidth * currentItem) + 'px';
-						}
-					});
+		this.clickButtonPrev = function(response) {
+			self.btnPrev.addEventListener('click', function() {
+				if (self.currentItem > 0) {
+					--self.currentItem;
+					document.querySelector('.content-slideshow ul').style.marginLeft = -(640 * self.currentItem) + 'px';
 				}
+			});
+		}
 
-				this.clickButtonNext = function() {
-					var itemWidth = document.querySelector('.content-slideshow ul li').offsetWidth;
-					self.btnNext.addEventListener('click', function() {
-						if (currentItem < quantItens) {
-							++currentItem;
-							document.querySelector('.content-slideshow ul').style.marginLeft = -(itemWidth * currentItem) + 'px';
-						}
-					});
+		this.clickButtonNext = function(response) {
+			self.btnNext.addEventListener('click', function() {
+				if (self.currentItem < self.quantItens) {
+					++self.currentItem;
+					document.querySelector('.content-slideshow ul').style.marginLeft = -(640 * self.currentItem) + 'px';
 				}
+			});
+		}
 
-				for (var i = 0; i < quantItens; i++) {
-					var item = document.createElement('li');
-					var link = document.createElement('a');
-					var photo = response.photos.photo[i];
-					var farmId = photo.farm;
-					var serverId = photo.server;
-					var id = photo.id;
-					var secret = photo.secret;
-					var url = 'http://farm' + farmId + '.staticflickr.com/' + serverId+ '/' + id+ '_' + secret;
+		this.clickThumb = function(response) {
+			var allThumbs = document.querySelectorAll('.content-thumbnails ul li'),
+				allThumbsArray = [];
 
-					document.querySelector('.content-thumbnails ul').appendChild(item);
-					item.appendChild(link);
-					link.setAttribute('href', url + medium);
-					link.innerHTML = '<img src="' + url + small + '" />';
-				};
+			allThumbs[0].setAttribute('class', 'active');
 
-				var allThumbs = document.querySelectorAll('.content-thumbnails ul li'),
-					allThumbsArray = [];
+			for (var i = 0; i < allThumbs.length; i++) {
+				allThumbsArray.push(allThumbs[i]);
+				allThumbs[i].addEventListener('click', function(e) {
+					var itemWidth = document.querySelector('.content-slideshow ul li').offsetWidth;
+					document.querySelector('.content-slideshow ul').marginLeft = -(i * itemWidth) + 'px';
+					window.scroll($('.content-slideshow'),0);
 
-				allThumbs[0].setAttribute('class', 'active');
+					this.setAttribute('class', 'active');
+					var siblings = this.parentNode.childNodes;
+					allSiblingsArray = [];
 
-				for (var i = 0; i < allThumbs.length; i++) {
-					allThumbsArray.push(allThumbs[i]);
-					allThumbs[i].addEventListener('click', function(e) {
-						var itemWidth = document.querySelector('.content-slideshow ul li').offsetWidth;
-						console.log(allThumbs.indexOff(0));
-						document.querySelector('.content-slideshow ul').marginLeft = -(i * itemWidth) + 'px';
-						window.scroll($('.content-slideshow'),0);
+					for (var i = 0; i < allThumbs.length; i++) {
+						allSiblingsArray.push(siblings[i]);
 
-						this.setAttribute('class', 'active');
-						var siblings = this.parentNode.childNodes;
-						allSiblingsArray = [];
+						if (allSiblingsArray[i] !== this) {
+							allSiblingsArray[i].removeAttribute('class');
+						}
+					};
+					e.preventDefault();
+				})
+			};
+		}
 
-						for (var i = 0; i < allThumbs.length; i++) {
-							allSiblingsArray.push(siblings[i]);
+		this.slideshow = function() {
+			//var itemWidth = document.querySelector('.content-slideshow ul li').offsetWidth,
+				item = document.createElement('li');
+			document.querySelector('.content-slideshow ul').appendChild(item);
+			item.innerHTML = '<img src="' + this.url + this.medium + '" />';
+			document.querySelector('.content-slideshow ul').style.width = 640 * this.quantItens + 'px';
+		}
+		this.thumbnails = function() {
+			var	item = document.createElement('li'),
+				link = document.createElement('a');
+			document.querySelector('.content-thumbnails ul').appendChild(item);
+			item.appendChild(link);
+			link.setAttribute('href', this.url + this.medium);
+			link.innerHTML = '<img src="' + this.url + self.small + '" />';
+		}
 
-							if (allSiblingsArray[i] !== this) {
-								allSiblingsArray[i].removeAttribute('class');
-							}
-						};
-						e.preventDefault();
-					})
-				};
-				this.clickButtonPrev();
-				this.clickButtonNext();
-			}
-		});
+		this.render = function(response) {
+			this.quantItens = response.photos.photo.length;
+
+			document.querySelector('.content-slideshow').innerHTML = '<ul></ul>';
+			document.querySelector('.content-thumbnails').innerHTML = '<ul></ul>';
+
+			for (var i = 0; i < this.quantItens; i++) {
+				this.photo = response.photos.photo[i];
+				this.farmId = this.photo.farm;
+				this.serverId = this.photo.server;
+				this.address = this.photo.id;
+				this.secret = this.photo.secret;
+				this.small = '_s.jpg'
+				this.medium = '_z.jpg'
+				this.url = 'http://farm' + this.farmId + '.staticflickr.com/' + this.serverId+ '/' + this.address + '_' + this.secret;
+				this.slideshow();
+				this.thumbnails();
+			};
+			this.clickButtonPrev();
+			this.clickButtonNext();
+			this.clickThumb();
+		}
+
+		this.ajax = function() {
+			$.ajax({
+				url: 'http://www.flickr.com/services/rest',
+				dataType: 'JSONP',
+				jsonp: 'jsoncallback',
+				data: {
+					method: 'flickr.photos.search',
+					api_key: 'df6f4b524cf35757c57c6f7418e11d01',
+					user_id: '26982917@N02',
+					format: 'json',
+				},
+				success: function(response){
+					self.render(response);
+				}
+			});
+		}
 	}
-
 	this.init();
 }
